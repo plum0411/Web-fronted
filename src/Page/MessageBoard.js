e0cbdd85cbb98085ec6024a9ed2a11f6c096c68c
plum_0411 <mayble0411@gmail.com>
2023-05-23 18:00:49 +0800

    board變漂亮!

--- src/Page/MessageBoard.js
+++ src/Page/MessageBoard.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { ChatBubbleOvalLeftEllipsisIcon, EllipsisHorizontalIcon } from '@heroicons/react/24/outline';
 import Header from '../component/Header';
-import { request, config } from '../api/setting';
+import { request, config, anonymousId } from '../api/setting';
 import { formatDateTime } from '../api/api';
 const MessageBoard = () => {
     const [posts, setPosts] = useState([]);
@@ -61,7 +61,7 @@ const MessageBoard = () => {
 
     const handleFormSubmit = (event) => {
         event.preventDefault();
-        const user_id = authUserId || 99999;
+        const user_id = authUserId || anonymousId;
         const url = `http://localhost:8000/api/posts/`;
 
         axios
@@ -137,8 +137,13 @@ const MessageBoard = () => {
 
     return (
         <div className="App font-poppins bg-white dark:bg-stone-900 h-full m-0 p-0 pt-16">
-            <div className='flex bg-yellow-600 w-full h-44 justify-center items-center'>
-                <div className='text-4xl text-white font-bold'>留言一下</div>
+            <div className=' bg-fixed 
+            bg-[url("http://www.transparenttextures.com/patterns/tree-bark.png")] 
+            bg-yellow-600 bg-img w-full sm:p-12 p-8 flex flex-col justify-center text-center text-white'>
+                <h1 className="text-2xl font-bold tracking-tight sm:text-4xl text-white">留言一下</h1>
+                <p className="mt-2 text-stone-100">
+                    到此一遊！你有甚麼想說的?
+                </p>
             </div>
             <Header />
             <div className="max-w-2xl mx-auto p-4 sm:p-6 lg:pt-16">
@@ -168,7 +173,7 @@ const MessageBoard = () => {
                             <div className="flex-1">
                                 <div className="flex justify-between items-start">
                                     <div>
-                                        <span className={post.user_id === 99999 ? 'font-bold text-gray-700' : 'font-bold text-amber-700'}>
+                                        <span className={post.user_id === anonymousId ? 'font-bold text-gray-700' : 'font-bold text-amber-700'}>
                                             {post.user.name}
                                         </span>
                                         <small className="ml-2 text-sm text-gray-600">{formatDateTime(post.created_at)}</small>
@@ -176,7 +181,7 @@ const MessageBoard = () => {
                                             <small className="text-sm text-gray-600"> · edited</small>
                                         )}
                                     </div>
-                                    {(post.user_id === authUserId || authUserId === null) && post.user_id !== 99999 && (
+                                    {(post.user_id === authUserId || authUserId === null) && post.user_id !== anonymousId && (
                                         <div className="relative inline-block text-left">
                                             <button
                                                 className="focus:outline-none w-6 h-6 text-gray-400 hover:text-gray-600"
